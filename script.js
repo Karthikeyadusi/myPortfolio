107546756173505ff99bbcfd9364e40d4c6018c3
DUSI VENKATA SAI KARTHIKEYA <dvskartikeya@gmail.com>
2024-10-08 16:51:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // scripts.js
+let visitorname = prompt('Hey there! What is your name?');
+let visitorName = visitorname ? visitorname.toUpperCase() : 'Visitor';
+if (visitorName) {
+    document.getElementById('visitorName').textContent = `Hello, ${visitorName}! This is My PORTFOLIO!`;
 
+}
 document.getElementById('darkModeToggle').addEventListener('change', function() {
     document.body.classList.toggle('dark-mode');
     document.querySelector('header').classList.toggle('dark-mode');
@@ -8,6 +13,7 @@ document.getElementById('darkModeToggle').addEventListener('change', function() 
     document.getElementById('projects').classList.toggle('dark-mode');
     document.getElementById('resume').classList.toggle('dark-mode');
     document.getElementById('contact').classList.toggle('dark-mode');
+    document.getElementById('visitorName').classList.toggle('dark-mode');
     // Toggle dark mode for contact icons
     document.querySelectorAll('.contact-icons a').forEach(function(icon) {
         icon.classList.toggle('dark-mode');
